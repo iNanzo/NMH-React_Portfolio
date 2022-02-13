4bd3703b5570fe680866203e68d8e4406482e750
Newton Hoang <newtonhoang99@gmail.com>
2022-02-12 19:38:13 -0800

    put html into components

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div id="about" class="about">
+        <h2>About Me</h2>
+        <img src="./assets/images/newton.jpg" alt="Photo of Newton Hoang"></img>
+        <p>A Seattle born Vietnamese-American, <br /> graduated from Bellevue College in 2021 with a degree in Digital Media. <br />Currently, i'm attending a UW Coding Bootcamp.</p>
+        </div>
+    );
+  }
+  
+  export default About;
---
+++ src/components/ContactMe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div class="contact">
+            <h2 id="contactme">Contact Me</h2>
+                <ul>
+                    <p>Email: <a href="mailto:newtonhoang99@gmail.com">newtonhoang99@gmail.com</a></p>
+                    <p>GitHub: <a href="https://github.com/iNanzo" target="_blank">iNanzo</a></p>
+                    <p id="lastcon">LinkedIn:  <a href="https://www.linkedin.com/in/newton-hoang-1327a2176/" target="_blank">Newton Hoang</a></p>
+                </ul>
+        </div>
+    );
+  }
+
+  export default Contact;
