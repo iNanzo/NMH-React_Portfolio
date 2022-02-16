746b1b5370d8049705ed536c588c70dce398401f
Newton Hoang <newtonhoang99@gmail.com>
2022-02-15 22:40:27 -0800

    images and footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,10 @@ import React from 'react';
 function Footer() {
     return (        
         <footer>
+            <ul class="footercolor">
+                <a class="footertext" href="https://github.com/iNanzo" target="_blank">GitHub</a>
+                <a class="footertext" href="https://www.linkedin.com/in/newton-hoang-1327a2176/" target="_blank">LinkedIn</a>
+            </ul>
         </footer>
     );
   }
--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -7,7 +7,7 @@ function Projects() {
             <div class="projects">
                 <section class="card mainproj">        
                     <h2>extracted.</h2>
-                    <a href="https://extracted-coffee.herokuapp.com/" target="_blank"><img src="./assets/images/Project4.png" alt="Extracted. Coffee Site"/></a><br />
+                    <a href="https://extracted-coffee.herokuapp.com/" target="_blank"><img src={require("../assets/images/Project4.png")} alt="Extracted. Coffee Site"/></a><br />
                     <p>
                         <a href="https://github.com/lyssg2/extracted" target="_blank">GitHub</a><br />
                         Extracted is a website that presents the best coffee shops in Seattle and Portland, <br />
@@ -16,7 +16,7 @@ function Projects() {
                 </section>
                 <section class="card">        
                     <h2>Note Taker</h2>
-                    <a href="https://newton-notes.herokuapp.com/" target="_blank"><img src="./assets/images/NoteTaker.PNG" alt="Note Taker"/></a><br />
+                    <a href="https://newton-notes.herokuapp.com/" target="_blank"><img src={require("../assets/images/NoteTaker.png")} alt="Note Taker"/></a><br />
                     <p>
                         <a href="https://github.com/iNanzo/NMH_BCHW11" target="_blank">GitHub</a><br />
                         A note taker deployed on Heroku. <br />
@@ -25,7 +25,7 @@ function Projects() {
                 </section>
                 <section class="card">        
                     <h2>Weather Dashboard</h2>
-                    <a href="https://inanzo.github.io/NMH_BCHW6/" target="_blank"><img src="./assets/images/Project2.PNG" alt="Weather Dashboard"/></a><br />
+                    <a href="https://inanzo.github.io/NMH_BCHW6/" target="_blank"><img src={require("../assets/images/Project2.PNG")} alt="Weather Dashboard"/></a><br />
                     <p>
                         <a href="https://github.com/iNanzo/NMH_BCHW6" target="_blank">GitHub</a><br />
                         A project with heavy focus on OpenWeather API usage. <br />
@@ -34,10 +34,18 @@ function Projects() {
                 </section>
                 <section class="card">        
                     <h2>Code Quiz</h2>
-                    <a href="https://inanzo.github.io/NMH_BCHW4/" target="_blank"><img src="./assets/images/Project3.PNG" alt="Code Quiz"/></a><br />
+                    <a href="https://inanzo.github.io/NMH_BCHW4/" target="_blank"><img src={require("../assets/images/Project3.PNG")} alt="Code Quiz"/></a><br />
                     <p>
                         <a href="https://github.com/iNanzo/NMH_BCHW4" target="_blank">GitHub</a><br />
                         A code quiz that makes use of append and localStorage functions.
+                    </p>
+                </section>
+                <section class="card">        
+                    <h2>Text Editor</h2>
+                    <a href="https://nmh-texteditor.herokuapp.com/" target="_blank"><img src={require("../assets/images/Project5.png")} alt="Note Taker"/></a><br />
+                    <p>
+                        <a href="https://github.com/iNanzo/NMH_BCHW19-Text_Editor" target="_blank">GitHub</a><br />
+                        A heroku deployed PWA text editor that can be used locally offline, or downloaded as a browser extension.
                     </p>
                 </section>
                 {/* <section class="card">        
@@ -47,7 +55,7 @@ function Projects() {
                 </section> */}
                 <section class="card mainproj">        
                     <h2>NBA Player Stats</h2>
-                    <a href="https://snkrfr3sh.github.io/sport_facts/" target="_blank"><img src="./assets/images/Project1.png" alt="NBA Player Stats"/></a><br />
+                    <a href="https://snkrfr3sh.github.io/sport_facts/" target="_blank"><img src={require("../assets/images/Project1.png")} alt="NBA Player Stats"/></a><br />
                     <p>
                         <a href="https://github.com/SnkrFr3sh/sport_facts" target="_blank">GitHub</a><br />
                         A site used to get quick basic information on NBA players past and present. <br />
